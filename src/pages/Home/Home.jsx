90d48a6bb09302fdf1a3e5dedc9f7e1ad45618a8
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-15 16:19:29 -0300

    Initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="bg-shop bg-cover flex flex-col justify-center items-center">
+      <h1 className="text-4xl">Fake Shop</h1>
+      <h2>This is a project trying to simulate a shopping page, no product is purchaseable here</h2>
+    </div>
+  );
+}
+
+export default Home;
