9ff478f64e28ca8ec3a3462cde70b685909a4d7f
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-16 15:45:51 -0300

    Fetch fake shop data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 import "./index.css";
@@ -6,6 +6,23 @@ import PropTypes from "prop-types";
 
 const App = ({ children }) => {
   const [cartHidden, setCartHidden] = useState(true);
+  const [productList, setProductList] = useState([]);
+  const [buyList, setBuyList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => {
+        if (res.status >= 400) {
+          throw new Error("server error");
+        }
+        return res.json()
+      })
+      .then(json => setProductList([json]))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false))
+  }, []);
 
   const handleCartClick = () => {
     if (cartHidden) {
@@ -14,6 +31,9 @@ const App = ({ children }) => {
       setCartHidden(true);
     }
   }
+
+  if (error) return <p>A network error was encountered</p>
+  if (loading) return <p>Loading...</p>
 
   return (
     <div className="">
