74e1c561ec5cac8842b134fc3fa521fd7dbe0725
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-16 11:55:38 -0300

    Add a page mask and style changes

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types"
+import { CloseRounded } from "@mui/icons-material";
 
 const ShoppingCart = ({ handleClick }) => {
   return (
-    <div className="absolute right-0 top-0 w-screen md:w-80 bg-black h-screen">
-      <button type="button" onClick={handleClick}>Close</button>
+    <div className="fixed top-0 flex h-screen w-screen">
+      <div className="origin-top-left animate-slidein bg-black opacity-50 flex-auto w-0" onClick={handleClick}></div>
+      <div className="origin-top-right animate-slidein w-screen md:w-80 bg-fuchsia-200">
+        <button className="m-2" type="button" onClick={handleClick}><CloseRounded /></button>
+      </div>
     </div>
   );
 }
