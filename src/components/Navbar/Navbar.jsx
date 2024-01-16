16e2189cb943c0648e1cacd95d5894f1140c5e1b
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-16 11:58:12 -0300

    Add cart icon and link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
+import { ShoppingCartRounded } from "@mui/icons-material"
 
 const Navbar = ({ handleClick }) => {
   return (
     <nav className="flex justify-between p-4 bg-slate-200 sticky top-0">
-      <Link to="/">FakeShop</Link>
-      <button type="button" onClick={handleClick}>Cart</button>
+      <Link className="font-bold italic" to="/">FakeShop</Link>
+      <button type="button" onClick={handleClick}><ShoppingCartRounded /></button>
     </nav>
   )
 }
