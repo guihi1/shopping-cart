721c6d5f02bb22bdb1e573c7c10003e495e417e7
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-16 11:52:40 -0300

    Add slidein animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ export default {
     extend: {
       backgroundImage: {
         'shop': "url('./src/assets/pexels-andrea-piacquadio-1050244.jpg')",
-      }
+      },
+      animation: {
+        slidein: 'slidein 0.7s ease-in 1',
+      },
+      keyframes: {
+        slidein: {
+          '0%': { transform: 'scaleX(0)' },
+          '100%': { transform: 'scaleX(1)' },
+        },
+      },
     },
   },
   plugins: [],
