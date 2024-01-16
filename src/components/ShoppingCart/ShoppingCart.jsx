79128d1e390245c681627ce6801b784405ba2c11
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-16 14:49:00 -0300

    Add checkout button

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -5,8 +5,11 @@ const ShoppingCart = ({ handleClick }) => {
   return (
     <div className="fixed top-0 flex h-screen w-screen">
       <div className="origin-top-left animate-slidein bg-black opacity-50 flex-auto w-0" onClick={handleClick}></div>
-      <div className="origin-top-right animate-slidein w-screen md:w-80 bg-fuchsia-200">
-        <button className="m-2" type="button" onClick={handleClick}><CloseRounded /></button>
+      <div className="flex flex-col items-center origin-top-right animate-slidein w-screen md:w-80 bg-fuchsia-200">
+        <div className="flex justify-start w-full">
+          <button className="m-2" type="button" onClick={handleClick}><CloseRounded /></button>
+        </div>
+        <button className="p-3 bg-sky-50 border-cyan-50 border-2 rounded-lg max-w-72" type="button">CHECKOUT</button>
       </div>
     </div>
   );
