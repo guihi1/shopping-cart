f29a2561c8dbed7852ab013342399694573e0086
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-17 11:02:35 -0300

    Add product quantity and style

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,26 @@ import PropTypes from 'prop-types'
 
 const Card = ({ title, price, image }) => {
   return (
-    <div>
-      <h3>{title}</h3>
-      <img src={image} alt="Product illustration" />
-      <p>{price}</p>
+    <div className="max-w-72 flex flex-col justify-center items-center text-center gap-3">
+      <h3 className="text-xl">{title}</h3>
+      <img className="w-60 h-auto" src={image} alt="Product illustration" />
+      <p className="text-xl">{price}</p>
+      <div className="flex gap-3">
+        <label htmlFor="qty">Quantity:</label>
+        <select name="qty" id="qty">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+      <button type="button">Add to cart</button>
     </div>
   );
 }
