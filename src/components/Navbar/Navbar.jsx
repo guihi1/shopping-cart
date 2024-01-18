8d1c40a063fdaa941cbce1a25dca8c75eab4d8e4
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-18 15:56:11 -0300

    Add animation when cart isn't empty

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,16 +2,23 @@ import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 import { ShoppingCartRounded } from "@mui/icons-material"
 
-const Navbar = ({ handleClick }) => {
+const Navbar = ({ cartList, handleClick }) => {
   return (
     <nav className="flex justify-between p-4 bg-slate-200 sticky top-0">
       <Link className="font-bold italic" to="/">FakeShop</Link>
-      <button type="button" onClick={handleClick}><ShoppingCartRounded /></button>
+      <button type="button" onClick={handleClick}>
+        {cartList.length > 0 ?
+          <div className="w-2 h-2 relative top-2 left-4 rounded-full bg-cyan-900 animate-ping"></div> :
+          null
+        }
+        <ShoppingCartRounded />
+      </button>
     </nav>
   )
 }
 
 Navbar.propTypes = {
+  cartList: PropTypes.array,
   handleClick: PropTypes.func,
 }
 
