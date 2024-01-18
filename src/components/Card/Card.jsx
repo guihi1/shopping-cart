62613993f5aebcef8dbd8abe9554f9fa52edb4fa
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-18 16:30:48 -0300

    Now changing qty reflects

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react';
 
-const Card = ({ title, price, image }) => {
+const Card = ({ id, title, price, image, handleAdding }) => {
+  const [qty, setQty] = useState(1);
   return (
-    <div className="max-w-72 flex flex-col justify-center items-center text-center gap-3">
-      <h3 className="text-xl">{title}</h3>
-      <img className="w-60 h-auto" src={image} alt="Product illustration" />
-      <p className="text-xl">{price}</p>
+    <div className="w-72 h-96 p-3 rounded-xl border-4 border-stone-400 bg-stone-100 flex flex-col justify-center items-center text-center gap-1">
+      <h3 className="text-xl h-14 overflow-hidden">{title}</h3>
+      <img className="w-auto h-44" src={image} alt="Product illustration" />
+      <p className="text-xl">${price}</p>
       <div className="flex gap-3">
-        <label htmlFor="qty">Quantity:</label>
-        <select name="qty" id="qty">
+        <label htmlFor={id}>Quantity:</label>
+        <select name="qty" id={id} onChange={(e) => setQty(e.target.value)}>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -21,16 +23,18 @@ const Card = ({ title, price, image }) => {
           <option value="10">10</option>
         </select>
       </div>
-      <button type="button">Add to cart</button>
+      <button className="rounded-xl bg-green-300 p-2 hover:bg-green-400" type="button" onClick={() => handleAdding(title, qty, image, price)}>Add to cart</button>
     </div>
   );
 }
 
 Card.propTypes = {
+  id: PropTypes.number,
   title: PropTypes.string,
   desc: PropTypes.string,
   price: PropTypes.number,
   image: PropTypes.string,
+  handleAdding: PropTypes.func,
 }
 
 export default Card
