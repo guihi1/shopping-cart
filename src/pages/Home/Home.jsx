eba228c6162962ce1e4ef4e2a149bc5282f89fda
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-19 15:52:04 -0300

    Increase backdrop blur

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="bg-shop bg-cover flex flex-col gap-5 justify-center items-center h-[calc(100vh-36px)]">
-      <h1 className="text-6xl m-5 backdrop-blur p-3 italic font-bold">Fake Shop</h1>
-      <h2 className="text-2xl m-5 backdrop-blur p-3">This is a project trying to simulate a shopping page, no product is purchasable here</h2>
-      <Link className="text-2xl m-5 border-2 border-indigo-500 bg-indigo-400 p-3 rounded-lg shadow-xl hover:scale-110 transition-transform duration-300 delay-75 hover:bg-indigo-600" to="/items">Shop now!</Link>
+      <h1 className="text-6xl m-5 backdrop-blur-2xl p-3 italic font-bold">Fake Shop</h1>
+      <h2 className="text-2xl m-5 backdrop-blur-3xl p-3 font-bold">This is a project trying to simulate a shopping page, no product is purchasable here</h2>
+      <Link className="text-2xl m-5 border-2 border-indigo-500 bg-indigo-400 p-3 rounded-lg shadow-xl hover:scale-110 transition-transform duration-300 delay-75 hover:bg-indigo-600" to="/shop">Shop now!</Link>
     </div>
   );
 }
