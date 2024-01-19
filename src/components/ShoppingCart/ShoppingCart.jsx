9377ff34f192ae49906d4acf8210a6bc77891b35
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-19 15:53:39 -0300

    Change colors show two decimals for price

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 import { CloseRounded } from "@mui/icons-material";
 import CartCard from "../CartCard/CartCard";
 
-const ShoppingCart = ({ cartList, handleClick }) => {
+const ShoppingCart = ({ cartList, handleClick, handleChange, handleRemove }) => {
   return (
     <div className="fixed top-0 flex h-screen w-screen">
       <div className="origin-top-left animate-slidein bg-black opacity-50 flex-auto w-0" onClick={handleClick}></div>
@@ -12,11 +12,11 @@ const ShoppingCart = ({ cartList, handleClick }) => {
         </div>
         <div className="flex-auto flex flex-col gap-3">
           {cartList.map((product) =>
-            <CartCard key={product.title} title={product.title} price={product.price} image={product.image} qty={product.qty} />
+            <CartCard key={product.title} title={product.title} price={product.price} image={product.image} qty={product.qty} handleChange={handleChange} handleRemove={handleRemove} />
           )}
         </div>
-        <h2>Total Price: ${cartList.reduce((acc, curr) => acc + curr.price * curr.qty, 0)}</h2>
-        <button className="m-2 p-3 bg-sky-50 border-cyan-50 border-2 rounded-lg max-w-72" type="button">CHECKOUT</button>
+        <h2>Total Price: ${(cartList.reduce((acc, curr) => acc + curr.price * curr.qty, 0)).toFixed(2)}</h2>
+        <button className="m-2 p-3 bg-sky-200 border-cyan-300 hover:bg-sky-400 hover:border-cyan-500 border-2 rounded-lg max-w-72" type="button">CHECKOUT</button>
       </div>
     </div>
   );
@@ -25,6 +25,8 @@ const ShoppingCart = ({ cartList, handleClick }) => {
 ShoppingCart.propTypes = {
   cartList: PropTypes.array,
   handleClick: PropTypes.func,
+  handleChange: PropTypes.func,
+  handleRemove: PropTypes.func,
 }
 
 export default ShoppingCart;
