0be268b25fa4504366cbe1554e4191aed824890b
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-01-19 15:49:51 -0300

    Allow sorting by category

--- src/pages/Shopping/Shopping.jsx
+++ src/pages/Shopping/Shopping.jsx
@@ -1,19 +1,38 @@
 import PropTypes from "prop-types"
+import { useState } from "react";
 import Card from "../../components/Card/Card";
 
-const Shopping = ({ productList }) => {
-  console.log(productList)
+const Shopping = ({ productList, handleAdding }) => {
+  const [category, setCategory] = useState("all");
+  console.log(productList);
   return (
-    <div>
-      {productList.map((product) =>
-        <Card key={product.id} title={product.title} price={product.price} image={product.image} />
-      )}
-    </div>
+    <>
+      <div className="m-5 text-center lg:m-10 lg:text-left">
+        <label htmlFor="category">Category: </label>
+        <select id="category" name="category" onChange={(e) => setCategory(e.target.value)}>
+          <option value="all">All</option>
+          <option value="men's clothing">Men&apos;s clothing</option>
+          <option value="jewelery">Jewelery</option>
+          <option value="electronics">Electronics</option>
+          <option value="women's clothing">Women&apos;s clothing</option>
+        </select>
+      </div>
+      <div className="flex flex-wrap gap-5 m-5 justify-center items-center">
+        {productList.filter((product) => {
+          if (category === product.category || category === "all") {
+            return true;
+          }
+        }).map((product) =>
+          <Card key={product.id} id={product.id} title={product.title} price={product.price} image={product.image} handleAdding={handleAdding} />
+        )}
+      </div>
+    </>
   );
 }
 
 Shopping.propTypes = {
   productList: PropTypes.array,
+  handleAdding: PropTypes.func,
 }
 
 export default Shopping;
